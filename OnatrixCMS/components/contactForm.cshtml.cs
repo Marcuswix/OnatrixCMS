735f90581b8ffd92639e9c3bbc1417d796b1ab88
Marcus <Marcuswix@yahoo.se>
2024-09-06 12:52:59 +0200

    "Our Services Section"

---
+++ OnatrixCMS/components/contactForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnatrixCMS.components
+{
+    public class contactFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
